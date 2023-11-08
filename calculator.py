528ee9d83e388d7be119abba1706d5b28062ae82
TheodoraManika <theodoramnk@gmail.com>
2023-11-08 20:58:22 +0200

    calculator

--- calculator.py
+++ calculator.py
@@ -1,1 +1,21 @@
 #Να φτίαζετε ένα πρόγρμμα που θα είναι κουμπιουτεράκι
+num1 = int(input("Give firest number"))
+num2 = int(input("Give second number"))
+symb = input("Give symbol")
+
+# +, -, *, /, %, **
+
+if symb == "+":
+    print(num1 + num2)
+elif symb == "-":
+    print(num1 - num2)
+elif symb == "*":
+    print(num1 * num2)
+elif symb == "/":
+    print(num1 / num2)
+elif symb == "%":
+    print(num1 % num2)
+elif symb == "**":
+    print(num1 ** num2)
+else:
+    print("wrong symbol")
