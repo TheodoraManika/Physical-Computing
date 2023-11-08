a13f07b43d96477cc3b3056b47e6f2bf5c43e7b1
TheodoraManika <theodoramnk@gmail.com>
2023-11-08 20:20:08 +0200

    8/11

--- do_i_know_you.py
+++ do_i_know_you.py
@@ -1,3 +1,11 @@
 #Γράψτε έναν κώδικα που αφού του δίνεις το όνομα σου να ελέγχει ένα σε ξέρει 
 #Εάν ναι να εμφανίζει I know you και το ονόμα 
 #Εάν όχι να εμφανίζε Who are you? και το όνομα
+
+my_name = "theodora"
+name = input("whats your name")
+
+if my_name == name:
+    print("hi there", my_name)
+else:
+    print("who are you?", name)
--- if_elif_else.py
+++ if_elif_else.py
@@ -1,1 +1,11 @@
 # Ας μάθουμε την δομή επιλογής 
+num = int(input("Give number"))
+
+if num == 22:
+    print("correct")
+elif num > 10:
+    print("you are close try again")
+elif num < 30:
+    print("you are close try again")
+else:
+    print("wrong number")
