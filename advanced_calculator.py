213cb64a0e1f117d2d9197acc4cd5898a92c04f9
TheodoraManika <theodoramnk@gmail.com>
2023-11-15 20:03:56 +0200

    advanced calculator

---
+++ advanced_calculator.py
@@ -0,0 +1,34 @@
+# State of the game if we contiue being in the app
+calc_state = "y"
+
+#loop for continuing being in the app
+while calc_state == "y":
+    #input the numbers and symbols
+    number1 = int(input("Give first number: \n"))
+    number2 = int(input("Give second number: \n"))
+    symbol = input("Give symbol: \n")
+
+    #do the right action based on the symbol we gave
+    if symbol == "+":
+        print(str(number1) + symbol + str(number2) + "= ", number1 + number2)
+    elif symbol == "-":
+        print(str(number1) + symbol + str(number2) + "= ", number1 - number2)
+    elif symbol == "*":
+        print(str(number1) + symbol + str(number2) + "= ", number1 * number2)
+    elif symbol == "/":
+        print(str(number1) + symbol + str(number2) + "= ", number1 / number2)
+    elif symbol == "//":
+        print(str(number1) + symbol + str(number2) + "= ", number1 // number2)
+    elif symbol == "%":
+        print(str(number1) + symbol + str(number2) + "= ", number1 % number2)
+    elif symbol == "**":
+        print(str(number1) + symbol + str(number2) + "= ", number1 ** number2)
+    else:
+        print("Wrong symbol. Try again")
+        continue
+
+    #Ask id they want to continue being in the calculator
+    calc_state = input("Do you want to continue? Type y or n \n")
+    if calc_state != "y" or calc_state != "n":
+        print("I dont understand. Try again")
+        calc_state = input("Do you want to continue? Type y or n \n")
