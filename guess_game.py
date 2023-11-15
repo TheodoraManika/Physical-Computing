64448f64f2b55ec494d7863e5dea5a4db398ab59
TheodoraManika <theodoramnk@gmail.com>
2023-11-15 19:44:45 +0200

    guess game exc

---
+++ guess_game.py
@@ -0,0 +1,17 @@
+import random
+
+secret_number = random.randint(1, 100)
+
+guess = input("Which number i am thinking")
+
+if secret_number == guess: 
+    print("You found it")
+elif secret_number > guess:
+    print("My number is greater")
+else:
+    print("My number is smaller")
+
+# Περιγράψτε τι κάνει ο κώδικας 
+
+# Γίνετε με κάποιο τρόπο να να μαντεύουμε συνεχώς μέχρι να το πετυχούμε;
+#Αν ναί πώς;
