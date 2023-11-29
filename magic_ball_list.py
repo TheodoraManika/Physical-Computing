c5aece0e3acd5b02c16476893e7db7c7e04b70bc
TheodoraManika <theodoramnk@gmail.com>
2023-11-29 21:07:26 +0200

    update

--- magic_ball_list.py
+++ magic_ball_list.py
@@ -1,11 +1,13 @@
 import random 
 
-answers = []
+answers = ["Yes", "No", "Maybe", "Dont know", "Mmmm...."]
 play = input("Do you wanna play this game y/n")
 
 while play != "n":
     question = input("Ask me something")
-
+    
+    r = random.choice(answers)
+    print(r)
         
     play = input("Do you wanna play this game y/n")
   
